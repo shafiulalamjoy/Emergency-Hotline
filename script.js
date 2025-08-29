bd194fd227dc0e74c455525bd8d374ff3295bf15
shafiulalamjoy <joyalam10@gmail.com>
2025-08-29 08:57:16 +0600

    added copy btn functionality

--- script.js
+++ script.js
@@ -14,11 +14,20 @@ buttons.forEach(function(btn) {
 
 // Call functionality with coin deduction
 let coins = 100; 
+let callHistory = [];
 
-    function makeCall(Name, colon, number) {
-      if (coins >= 20) {
+function makeCall(Name, colon, number) {
+  if (coins >= 20) {
         coins -= 20;
         document.getElementById("coinCount").textContent = coins;
+  const call ={ 
+    name: Name,
+    number: number,
+    time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit',second:`2-digit`, hour12: true }).toLocaleUpperCase(),
+  };
+  callHistory.push(call);
+  updateCallHistory();
+  
         alert("📞Calling"+ Name + colon + number);
       } else {
         alert("Insufficient coins to make the call.");
@@ -27,7 +36,44 @@ let coins = 100;
 
 // Call history functionality
 
+function updateCallHistory() {
+  const historyList = document.getElementById("callHistory");
+  historyList.innerHTML = "";
 
+  callHistory.slice().reverse().forEach((call) => {
+    let div = document.createElement("div");
+    div.className =
+      "flex justify-between items-center bg-white shadow rounded p-2 mb-2 text-sm";
+
+    div.innerHTML = `
+      <div>
+        <p class="font-semibold">${call.name}</p>
+        <p class="text-gray-500">${call.number}</p>
+      </div>
+      <span class="text-sm">${call.time}</span>
+    `;
+
+    historyList.appendChild(div);
+  });
+}
+
+// Clear Call History
+function clearHistory() {
+  callHistory = [];
+  updateCallHistory();
+}
+
+// copy button functionality
+
+let copyCounter = 0;
+
+function copyBtn(number) {
+  navigator.clipboard.writeText(number).then(() => {
+    copyCounter++;
+    document.getElementById("copyCount").textContent = copyCounter;
+    alert("Copied: " + number);
+  });
+}
 
 
 
